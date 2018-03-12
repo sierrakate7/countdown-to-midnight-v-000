7eadd2e117e2e67cfdabbe158fc4cb6ee958270d
Sierra McCahon <smitche4@mail.umw.edu>
2018-03-12 19:22:19 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,23 @@
-#write your code here
-
-def countdown
-  number = 10
-  while number > 0 
-  puts "{number} SECOND(S)!"
-  num -= 1 
-  puts "HAPPY NEW YEAR"
-end
-end
++def countdown(num)
++
++while num > 0
++
++  puts "#{num} SECOND(S)!"
++  num -= 1
++
++end
++  "HAPPY NEW YEAR!"
++end
++
++
++def countdown_with_sleep(num)
++
++while num > 0
++
++  puts "#{num} SECOND(S)!"
++  sleep(1)
++  num -= 1
++
++end
++  "HAPPY NEW YEAR!"
+ end	 end
