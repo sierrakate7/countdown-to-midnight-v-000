5139c1c388801c4d8206760ab465518eab4ba4bb
Sierra McCahon <smitche4@mail.umw.edu>
2018-03-12 19:27:54 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,10 @@ def countdown(number)
 end
 
 def countdown_with_sleep(number)
+  while number > 0
+   sleep 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
